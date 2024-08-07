4f3364e8afa37c9c442dff8cad14395c6e439011
extraclowny <lemonade584521@gmail.com>
2024-08-07 13:39:33 -0700

    booking and menu views added

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import menuview, bookingview
 
 urlpatterns = [
     path('comments', views.form_view),
+    path('booking', bookingview.as_view()),
+    path('menu', menuview.as_view()),
 ]
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .forms import CommentForm
-from .models import UserComment
+from .models import UserComment, Booking, Menu
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 def form_view(request):
     form = CommentForm()
@@ -20,3 +24,25 @@ def form_view(request):
                 'message': 'success'
             })
     return render(request, 'blog.html', {'form': form})
+
+class bookingview(APIView):
+    def get(self,request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many = True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def post(self, request):
+        serializer = BookingSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+class menuview(APIView):
+    def get(self, request):
+        items = Menu.objects.all()
+        serializer = MenuSerializer(items, many = True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def post(self, request):
+        serializer = BookingSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
